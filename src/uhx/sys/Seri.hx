1d190931655a71bdababa55bee45a88c26d2219b
Skial Bainn <skial.bainn@gmail.com>
2015-02-11 14:39:21 +0000

    Nothing to see.

--- src/uhx/sys/Seri.hx
+++ src/uhx/sys/Seri.hx
@@ -1,16 +1,29 @@
 package uhx.sys;
 
+import haxe.io.Input;
+import haxe.io.Output;
+import sys.FileSystem;
+
+using sys.io.File;
+using haxe.io.Path;
+using sys.FileSystem;
+
 /**
  * ...
  * @author Skial Bainn
  * Haitian Creole for range
  */
+
+@:enum abstract UnicodeVersion(String) from String to String {
+	public var v700 = '7.0.0';
+}
+ 
 class Seri {
 	
 	/**
 	 * The Unicode version the data is base on.
 	 */
-	public static var version = '7.0.0';
+	public static var version:UnicodeVersion = '7.0.0';
 
 	public function new() {
 		
