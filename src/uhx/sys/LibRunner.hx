c48140084c7b64caa3e3d4cb0ef2d7c1c0b7ddf1
Skial Bainn <skial.bainn@gmail.com>
2015-02-12 14:46:17 +0000

    Parse Scripts.txt.gz and Blocks.txt.gz and extract _some_ information.

--- src/uhx/sys/LibRunner.hx
+++ src/uhx/sys/LibRunner.hx
@@ -6,6 +6,7 @@ import haxe.io.Output;
 import sys.FileSystem;
 import format.gz.Reader;
 
+using StringTools;
 using sys.io.File;
 using haxe.io.Path;
 using sys.FileSystem;
@@ -40,12 +41,20 @@ using sys.FileSystem;
 	@alias('s')
 	public var scripts:Bool = false;
 	
+	@alias('b')
+	public var blocks:Bool = false;
+	
 	private var _data:String = '';
+	private var _blocks:String = '';
 	private var _scripts:String = '';
 	
 	private var codepoints:Array<String> = [];
 	
 	private var dataParts:Array<String> = [];
+	private var blockParts:Array<String> = [];
+	private var scriptParts:Array<String> = [];
+	
+	private var unicodeBlocks:Array<String> = [];
 	private var unicodeScripts:Array<String> = [];
 	private var unicodeCategories:Array<String> = [];
 	
@@ -58,18 +67,26 @@ using sys.FileSystem;
 		trace( unicodeCategories );
 		trace( unicodeScripts );
 		trace( codepoints );
+		trace( unicodeBlocks );
 	}
 	
 	private function load():Void {
-		var dataPath = '$resource/UnicodeData.txt.gz'.normalize();
+		var blockPath = '$resource/Blocks.txt.gz'.normalize();
 		var scriptPath = '$resource/Scripts.txt.gz'.normalize();
+		var dataPath = '$resource/UnicodeData.txt.gz'.normalize();
 		
-		if (dataPath.exists() && scriptPath.exists()) {
+		if (dataPath.exists() && scriptPath.exists() && blockPath.exists()) {
+			// Easy, seperated by `;`
 			_data = new Reader( dataPath.read() ).read().data.toString();
 			dataParts = _data.split(';');
 			
-			_scripts = new Reader( scriptPath.read() ).read().data.toString();
+			// Blocks of text starting with `#`, with lines containing `;` and `#' between info :/
+			_scripts = sanitize( new Reader( scriptPath.read() ).read().data.toString() ).replace('#', ';');
+			scriptParts = _scripts.split(';');
 			
+			// Block of text starting with `#', seperated by `;` and `\n`. `\n` is replaced by `;`.
+			_blocks = sanitize( new Reader( blockPath.read() ).read().data.toString() );
+			blockParts = _blocks.split(';');
 		}
 	}
 	
@@ -77,17 +94,13 @@ using sys.FileSystem;
 		var length = dataParts.length;
 		var index = 0;
 		
-		while (index < length-1) {
+		// UnicodeData.txt
+		while (index < length - 1) {
 			for (i in index...(index + 14)) {
 				
 				// Builds an array of unicode classes.
 				if (categories && (i - index) == 2 && unicodeCategories.indexOf( dataParts[i] ) == -1) {
 					unicodeCategories.push( dataParts[i] );
-					
-				}
-				
-				if (scripts && (i - index) == 3 && unicodeScripts.indexOf( dataParts[i] ) == -1) {
-					unicodeScripts.push( dataParts[i] );
 					
 				}
 				
@@ -101,6 +114,43 @@ using sys.FileSystem;
 			// Move to the next row.
 			index += 14;
 		}
+		
+		// Scripts.txt
+		length = scriptParts.length;
+		index = 0;
+		
+		if (scripts) while (index < length - 1) {
+			for (i in index...(index + 3)) {
+				if ((i - index) == 1 && unicodeScripts.indexOf( scriptParts[i].trim() ) == -1) {
+					unicodeScripts.push( scriptParts[i].trim() );
+					
+				}
+				
+			}
+			
+			index += 3;
+			
+		}
+		
+		// Blocks.txt
+		length = blockParts.length;
+		index = 0;
+		
+		if (blocks) while (index < length - 1) {
+			for (i in index...(index + 2)) {
+				// The second check isnt needed at the time of writing.
+				if ((i - index) == 1 && unicodeBlocks.indexOf( blockParts[i].trim() ) == -1) unicodeBlocks.push( blockParts[i].trim() );
+				
+			}
+			
+			// Move to the next row.
+			index += 2;
+			
+		}
+	}
+	
+	private function sanitize(value:String):String {
+		return value.split('\n').filter( function(s) return !s.startsWith('#') && s.trim() != '' ).join(';');
 	}
 	
 }
