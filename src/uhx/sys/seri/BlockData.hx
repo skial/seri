979f57521f1a095e8832640c1cba3b1a5c784992
Skial Bainn <skial.bainn@gmail.com>
2015-02-13 13:54:52 +0000

    Add BlockData.hx which wraps data from Blocks.txt

---
+++ src/uhx/sys/seri/BlockData.hx
@@ -0,0 +1,36 @@
+package uhx.sys.seri;
+
+import uhx.sys.seri.Range;
+
+using Std;
+using StringTools;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+@:forward abstract BlockData(Array<String>) from Array<String> to Array<String> {
+
+	public var range(get, never):Range;
+	public var block(get, never):String;
+	
+	public inline function new(v:Array<String>) {
+		this = v;
+	}
+	
+	private inline function get_range():Range {
+		return this[0].indexOf('..') == -1 
+			? { 
+				min: '0x${this[0].trim()}'.parseInt(), 
+				max: '0x${this[0].trim()}'.parseInt() 
+			} : {
+				min: '0x${this[0].split("..")[0]}'.parseInt(),
+				max: '0x${this[0].split("..")[1].trim()}'.parseInt()
+			}
+	}
+	
+	private inline function get_block():String {
+		return this[1].trim();
+	}
+	
+}
