48cc16ad4b064a628ce6416755dfae77032c8e29
Skial Bainn <skial.bainn@gmail.com>
2015-02-20 13:24:02 +0000

    Change version from String to Version abstract.

--- template/Unicode.hx
+++ template/Unicode.hx
@@ -1,12 +1,14 @@
 package uhx.sys.seri.$package;
 
+import uhx.sys.seri.Version;
+
 /**
  * ...
  * @author Skial Bainn
  */
 class Unicode {
 
-	public static var version:String = "$version";
+	public static var version:Version = "$version";
 	public static var blocks:Array<String> = [$blocks];
 	public static var scripts:Array<String> = [$scripts];
 	public static var categories:Array<String> = [$categories];
