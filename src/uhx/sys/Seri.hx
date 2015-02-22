4538424686337df5d38fbe8109b6adf8c6e64171
Skial Bainn <skial.bainn@gmail.com>
2015-02-22 16:10:43 +0000

    Macro method will fetch codepoints based on the passed category ie Zs. Currently the values are cached but the returned expression is an inline array of codepoint values.

--- src/uhx/sys/Seri.hx
+++ src/uhx/sys/Seri.hx
@@ -1,12 +1,16 @@
 package uhx.sys;
 
+import haxe.ds.StringMap;
+import haxe.Json;
 import uhx.sys.seri.*;
 
 #if sys
+import uhx.sys.Ioe;
 import haxe.io.Input;
 import haxe.io.Output;
 import sys.FileSystem;
 import sys.io.Process;
+import uhx.sys.ExitCode;
 #end
 
 #if macro
@@ -27,7 +31,7 @@ using sys.FileSystem;
  * @author Skial Bainn
  * Haitian Creole for range
  */
-class Seri {
+@:access(uhx.sys.Ioe) class Seri {
 	
 	/**
 	 * The latest unicode version the data is base on.
@@ -35,15 +39,41 @@ class Seri {
 	@:isVar public static var version(get, set):Version = '7.0.0';
 	
 	#if macro
+	private static var ioe:Ioe = new Ioe();
 	private static var printer:Printer = new Printer();
 	private static var process:Process;
+	private static var codepointCache:StringMap<Array<Expr>> = new StringMap();
 	#end
 	
 	public static macro function getCategory(category:ExprOf<String>):ExprOf<Array<CodePoint>> {
 		if (category.expr.match(EConst(CString(_)))) {
-			//process = new Process('seri', ['-r', 'res/$version/', '-c', printer.printExpr( category )]);
-			return macro [];
+			// Turn the expression into a string, but remove the quotes. `"Zs"` => `Zs`.
+			var value = printer.printExpr( category ).substring(1, 3);
+			var results = [];
+			
+			if (!codepointCache.exists( value )) {
+				// Open a new process to seri asking for all codepoints in `value`.
+				process = new Process('haxelib', ['run', 'seri', '-c', value]);
+				
+				// Let the Ioe instance handle the pipes.
+				ioe.process( process.stdout, process.stdin );
+				var json:Results = Json.parse( ioe.content );
+				
+				// The return value needs to be an expression.
+				results = json.codepoints.map( toExpression );
+				
+				// And store the expression as its never been fetched before.
+				codepointCache.set( value, results );
+				
+			} else {
+				results = codepointCache.get( value );
+				
+			}
+			
+			return macro $a { results };
+			
 		} else {
+			// If `category` was not a constant string, let the runtime method handle it.
 			return macro Seri._getCategory($category);
 			
 		}
@@ -60,5 +90,9 @@ class Seri {
 	private static function set_version(v:String):String {
 		return version = v;
 	}
+	
+	#if macro
+	public static function toExpression<T>(v:T):Expr return macro $v { v };
+	#end
 	
 }
