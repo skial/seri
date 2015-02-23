b5ff1c3c4cb7613a68f19f99de57b9205600ae04
Skial Bainn <skial.bainn@gmail.com>
2015-02-23 14:29:33 +0000

    Minor

--- src/uhx/sys/seri/Version.hx
+++ src/uhx/sys/seri/Version.hx
@@ -4,6 +4,6 @@ package uhx.sys.seri;
  * ...
  * @author Skial Bainn
  */
-@:enum abstract Version(String) from String to String {
+@:forward @:enum abstract Version(String) from String to String {
 	public var v700 = '7.0.0';
 }
