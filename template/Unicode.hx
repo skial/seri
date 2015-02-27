afe0f9545478fe1be81102e959b8406c98da120a
Skial Bainn <skial.bainn@gmail.com>
2015-02-27 13:56:32 +0000

    When retyping, all fields need to exist in the original class.

--- template/Unicode.hx
+++ template/Unicode.hx
@@ -1,5 +1,6 @@
 package uhx.sys.seri.$package;
 
+import haxe.ds.StringMap;
 import uhx.sys.seri.Version;
 
 /**
@@ -13,5 +14,9 @@ class Unicode implements Klas {
 	public static var blocks:Array<String> = [$blocks];
 	public static var scripts:Array<String> = [$scripts];
 	public static var categories:Array<String> = [$categories];
+	
+	public static var codePoints:StringMap<Array<CodePoint>> = new StringMap();
+	public static var blockPoints:StringMap<Array<CodePoint>> = new StringMap();
+	public static var scriptPoints:StringMap<Array<CodePoint>> = new StringMap();
 	
 }
