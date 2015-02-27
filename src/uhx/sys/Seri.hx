919513b9848a9e9e943683801492d5cea2b06937
Skial Bainn <skial.bainn@gmail.com>
2015-02-27 13:57:02 +0000

    Modify to use `uhx.macro.KlasImp.RETYPE`.

--- src/uhx/sys/Seri.hx
+++ src/uhx/sys/Seri.hx
@@ -16,6 +16,7 @@ import uhx.sys.ExitCode;
 #if macro
 import haxe.macro.Type;
 import haxe.macro.Expr;
+import uhx.macro.KlasImp;
 import haxe.macro.Printer;
 import haxe.macro.Context;
 #end
@@ -41,33 +42,66 @@ using StringTools;
 	@:isVar public static var version(get, set):Version = '7.0.0';
 	
 	#if macro
+	private static var process:Process;
 	private static var ioe:Ioe = new Ioe();
 	private static var printer:Printer = new Printer();
-	private static var process:Process;
 	private static var codepointCache:StringMap<Array<Expr>> = new StringMap();
+	
+	public static var requestedBlocks:StringMap<Array<String>> = new StringMap();
+	public static var requestedScripts:StringMap<Array<String>> = new StringMap();
 	public static var requestedCategories:StringMap<Array<String>> = new StringMap();
 	#end
 	
 	public static macro function getCategory(category:ExprOf<String>):ExprOf<Array<CodePoint>> {
+		var result = macro Seri._getCategory( $category );
+		
 		if (category.expr.match(EConst(CString(_)))) {
 			// Turn the expression into a string, but remove the quotes. `"Zs"` => `Zs`.
-			var value = printer.printExpr( category ).substring(1, 3);
+			var value = printer.printExpr( category );
+			value = value.substring(1, value.length - 1);
 			var results = [];
 			
 			if (requestedCategories.exists( version )) results = requestedCategories.get( version );
 			if (results.indexOf( value ) == -1) results.push( value );
 			
 			requestedCategories.set( version, results );
-			return macro $p { ['uhx', 'sys', 'seri', 'v${version.replace(".", "")}', 'Unicode', 'codepoints', 'get'] }( $v{ value } );
+			
+			// Trigger a rebuild of Unicode.hx
+			KlasImp.retype( ['uhx', 'sys', 'seri', 'v${version.replace(".", "")}', 'Unicode'].join('.'), ':unicode' );
+			
+			// Bypass `Seri._getCategory`.
+			result = macro $p { ['uhx', 'sys', 'seri', 'v${version.replace(".", "")}', 'Unicode', 'codePoints', 'get'] }( $v{ value } );
+			
+		}
+		
+		return result;
+	}
+	
+	public static function _getCategory(category:String):Array<CodePoint> {
+		return uhx.sys.seri.v700.Unicode.codePoints.get(category);
+	}
+	
+	public static macro function getScript(script:ExprOf<String>):ExprOf<Array<CodePoint>> {
+		if (script.expr.match(EConst(CString(_)))) {
+			
+			var value = printer.printExpr( script );
+			value = value.substring(1, value.length - 1);
+			var results = [];
+			
+			if (requestedScripts.exists( version )) results = requestedScripts.get( version );
+			if (results.indexOf( value ) == -1) results.push( value );
+			
+			requestedScripts.set( version, results );
+			return macro $p { ['uhx', 'sys', 'seri', 'v${version.replace(".", "")}', 'Unicode', 'scriptsPoints', 'get'] }( $v{ value } );
 			
 		} else {
 			// If `category` was not a constant string, let the runtime method handle it.
-			return macro Seri._getCategory($category);
+			return macro Seri._getScript($script);
 			
 		}
 	}
 	
-	public static function _getCategory(category:String):Array<CodePoint> {
+	public static function _getScript(script:String):Array<CodePoint> {
 		return [];
 	}
 	
