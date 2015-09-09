0c156a97160d83a6c6adaefcd927d4b98292ba9d
Skial Bainn <skial.bainn@gmail.com>
2015-09-09 15:12:01 +0100

    Remove macro methods. Update method return types.

--- src/uhx/sys/Seri.hx
+++ src/uhx/sys/Seri.hx
@@ -4,36 +4,6 @@ import haxe.Json;
 import uhx.sys.seri.*;
 import haxe.ds.StringMap;
 
-#if sys
-import uhx.sys.Ioe;
-import haxe.io.Input;
-import haxe.io.Output;
-import sys.FileSystem;
-import sys.io.Process;
-import uhx.sys.ExitCode;
-#end
-
-#if macro
-import haxe.macro.Type;
-import haxe.macro.Expr;
-import uhx.macro.KlasImp;
-import haxe.macro.Printer;
-import haxe.macro.Context;
-import haxe.macro.Compiler;
-import uhx.macro.klas.TypeInfo;
-#end
-
-#if sys
-using sys.io.File;
-using haxe.io.Path;
-using sys.FileSystem;
-#end
-
-#if macro
-using haxe.macro.ExprTools;
-using haxe.macro.MacroStringTools;
-#end
-
 using StringTools;
 
 /**
@@ -48,52 +18,17 @@ using StringTools;
 	 */
 	@:isVar public static var version(get, set):Version = '7.0.0';
 	
-	#if macro
-	private static var process:Process;
-	private static var ioe:Ioe = new Ioe();
-	private static var printer:Printer = new Printer();
-	private static var codepointCache:StringMap<Array<Expr>> = new StringMap();
-	
-	public static var requestedBlocks:StringMap<Array<String>> = new StringMap();
-	public static var requestedScripts:StringMap<Array<String>> = new StringMap();
-	public static var requestedCategories:StringMap<Array<String>> = new StringMap();
-	#end
-	
-	#if display
-	public static function getCategory(category:String):Array<CodePoint> return [];
-	#else
-	public static macro function getCategory(category:ExprOf<String>):ExprOf<Array<CodePoint>> {
-		return processExpr( category, 'Category', requestedCategories );
-	}
-	
-	public static function _getCategory(category:String):Array<CodePoint> {
+	public static function getCategory(category:String):Array<Range> {
 		return uhx.sys.seri.v700.Unicode.categoryPoints.get(category);
 	}
-	#end
 	
-	#if display
-	public static function getScript(script:String):Array<CodePoint> return [];
-	#else
-	public static macro function getScript(script:ExprOf<String>):ExprOf<Array<CodePoint>> {
-		return processExpr( script, 'Script', requestedScripts );
-	}
-	
-	public static function _getScript(script:String):Array<CodePoint> {
+	public static function getScript(script:String):Null<Range> {
 		return uhx.sys.seri.v700.Unicode.scriptPoints.get(script);
 	}
-	#end
 	
-	#if display
-	public static function getBlock(block:String):Array<CodePoint> return [];
-	#else
-	public static macro function getBlock(block:ExprOf<String>):ExprOf<Array<CodePoint>> {
-		return processExpr( block, 'Block', requestedBlocks );
-	}
-	
-	public static function _getBlock(block:String):Array<CodePoint> {
+	public static function getBlock(block:String):Null<Range> {
 		return uhx.sys.seri.v700.Unicode.blockPoints.get(block);
 	}
-	#end
 	
 	private static function get_version():String {
 		return version;
@@ -102,62 +37,5 @@ using StringTools;
 	private static function set_version(v:String):String {
 		return version = v;
 	}
-	
-	#if macro
-	public static function toExpression<T>(v:T):Expr return macro $v { v };
-	
-	@:access(uhx.macro.KlasImp) private static function processExpr(expr:Expr, topic:String, map:StringMap<Array<String>>):Expr {
-		var result = macro $p { ['Seri', '_get$topic'] } ( $expr );
-		var lVars = Context.getLocalTVars();
-		var lMethod = Context.getLocalMethod();
-		var lClass = '' + Context.getLocalClass();
-		var path = ['uhx', 'sys', 'seri', 'v${version.replace(".", "")}', 'Unicode'];
-		
-		KlasImp.initialize();
-		
-		switch (expr.expr) {
-			case EConst(CString(value)):
-				var results = map.exists( version ) ? map.get( version ) : [];
-				if (results.indexOf( value ) == -1) results.push( value );
-				
-				map.set( version, results );
-				
-				var parts = lClass.split('.');
-				
-				KlasImp.triggerRebuild( path.join('.'), ':unicode' );
-				
-				result = macro $p { path.concat( [ '${topic.toLowerCase()}Points', 'get' ] ) } ($v { value } );
-				
-			case EConst(CIdent(id)) if (lVars.exists( id )):
-				// Look for variable declarations that might contain a constant 
-				// string value I can use to populate the Unicode class with.
-				KlasImp.inspect( lClass, searchForVariable.bind(_, _, lMethod, id, processExpr.bind(_, topic, map)) );
-				
-			case _:
-				
-				
-		}
-		
-		return result;
-	}
-	
-	private static function searchForVariable(type:Type, fields:Array<Field>, field:String, variable:String, callback:Expr->Expr):Void {
-		for (_field in fields) if (_field.name == field) switch (_field.kind) {
-			case FFun(method) if (method != null): method.expr.iter( matchVariable.bind(_, variable, callback) );
-			case _: 
-		}
-	}
-	
-	private static function matchVariable(expr:Expr, variable:String, callback:Expr->Expr):Void {
-		switch (expr) {
-			case macro var $name = $value if (name == variable):
-				callback(value);
-				
-			case _:
-				expr.iter( matchVariable.bind(_, variable, callback) );
-				
-		}
-	}
-	#end
 	
 }
