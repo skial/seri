6e32e906a30bff3b5abf46d728ada300d0b61584
Skial Bainn <skial.bainn@gmail.com>
2015-09-10 09:00:53 +0100

    @forward => @:forward. Forward underlying operator overloads. Remove @:noCompletion meta.

--- src/uhx/sys/seri/CodePoint.hx
+++ src/uhx/sys/seri/CodePoint.hx
@@ -4,22 +4,38 @@ package uhx.sys.seri;
  * ...
  * @author Skial Bainn
  */
-@forward abstract CodePoint(unifill.CodePoint) from unifill.CodePoint to unifill.CodePoint {
+@:forward abstract CodePoint(unifill.CodePoint) from unifill.CodePoint to unifill.CodePoint {
 
 	public inline function new(v:unifill.CodePoint) {
 		this = v;
 	}
 	
-	@:access( unifill.CodePoint ) @:noCompletion @:from public static inline function fromInt(v:Int):CodePoint {
+	@:access( unifill.CodePoint ) @:from public static inline function fromInt(v:Int):CodePoint {
 		return new unifill.CodePoint( v );
 	}
 	
-	@:noCompletion @:to public inline function toInt():Int {
+	@:to public inline function toInt():Int {
 		return this.toInt();
 	}
 	
-	@:noCompletion @:to public inline function toString():String {
+	@:to public inline function toString():String {
 		return this.toString();
 	}
+	
+	/**
+	 * Manually forward `unifill.CodePoint` operator overloads.
+	 * Why is there no obvious builtin way of doing this?
+	 */
+	@:op(A + B) static function cons(a:CodePoint, b:String):String;
+	@:op(A + B) static function snoc(a:String, b:CodePoint):String;
+	@:op(A + B) static function addInt(a:CodePoint, b:Int):CodePoint;
+	@:op(A - B) static function sub(a:CodePoint, b:CodePoint):Int;
+	@:op(A - B) static function subInt(a:CodePoint, b:Int):CodePoint;
+	@:op(A == B) static function eq(a:CodePoint, b:CodePoint):Bool;
+	@:op(A != B) static function ne(a:CodePoint, b:CodePoint):Bool;
+	@:op(A < B) static function lt(a:CodePoint, b:CodePoint):Bool;
+	@:op(A <= B) static function lte(a:CodePoint, b:CodePoint):Bool;
+	@:op(A > B) static function gt(a:CodePoint, b:CodePoint):Bool;
+	@:op(A >= B) static function gte(a:CodePoint, b:CodePoint):Bool;
 	
 }
