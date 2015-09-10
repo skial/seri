2fa97d9c1630580c348e062809c3c207eb16fddd
Skial Bainn <skial.bainn@gmail.com>
2015-09-10 14:02:50 +0100

    Add Unicode v7.0.0 Block.hx, Script.hx and Category.hx abstract enums.

---
+++ src/uhx/sys/seri/v700/Category.hx
@@ -0,0 +1,41 @@
+package uhx.sys.seri.v700;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+@:enum 
+@:unicode("7.0.0") 
+abstract Category(String) from String to String {
+	
+	public var Cc = "Cc";
+	public var Zs = "Zs";
+	public var Po = "Po";
+	public var Sc = "Sc";
+	public var Ps = "Ps";
+	public var Pe = "Pe";
+	public var Sm = "Sm";
+	public var Pd = "Pd";
+	public var Nd = "Nd";
+	public var Lu = "Lu";
+	public var Sk = "Sk";
+	public var Pc = "Pc";
+	public var Ll = "Ll";
+	public var So = "So";
+	public var Lo = "Lo";
+	public var Pi = "Pi";
+	public var Cf = "Cf";
+	public var No = "No";
+	public var Pf = "Pf";
+	public var Lt = "Lt";
+	public var Lm = "Lm";
+	public var Mn = "Mn";
+	public var Me = "Me";
+	public var Mc = "Mc";
+	public var Nl = "Nl";
+	public var Zl = "Zl";
+	public var Zp = "Zp";
+	public var Cs = "Cs";
+	public var Co = "Co";;
+	
+}
