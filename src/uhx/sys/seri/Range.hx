91c39f5462a3c70122980231e6653e76c3e69756
Skial Bainn <skial.bainn@gmail.com>
2015-09-10 09:50:15 +0100

    Add iterator, which iterates over each codepoint between `min` and `max`. Add `has` to check if codepoint exists within the range.

--- src/uhx/sys/seri/Range.hx
+++ src/uhx/sys/seri/Range.hx
@@ -15,8 +15,36 @@ class Range {
 		this.max = max;
 	}
 	
+	public inline function has(value:Int):Bool {
+		return value >= min && value <= max;
+	}
+	
+	public function iterator():Iterator<CodePoint> {
+		return new RangeIterator( this );
+	}
+	
 	private function get_length():Int {
 		return (max - min) + 1;
+	}
+	
+}
+
+private class RangeIterator {
+	
+	public var range:Range;
+	public var current:CodePoint;
+	
+	public inline function new(range:Range) {
+		this.range = range;
+		this.current = range.min;
+	}
+	
+	public function next():CodePoint {
+		return current = current.toInt() + 1;
+	}
+	
+	public function hasNext():Bool {
+		return current <= range.max;
 	}
 	
 }
